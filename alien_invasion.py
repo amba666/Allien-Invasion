3c995a2b59c3a24bd0580bb22a885b7cec3181fa
Amba <67413568+amba666@users.noreply.github.com>
2024-03-03 22:00:20 +0300

    a-i game adding bg color

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,6 +11,10 @@ class AllienInvasion:
             self.screen = pygame.display.set_mode((1200, 800))
             pygame.display.set_caption("Allien Invasion")
 
+            #set bg-color
+            self.bg_color = (230, 230, 230)
+
+
       def  run_game(self):
             """Start the main loop for the game"""
             while True:
@@ -18,6 +22,9 @@ class AllienInvasion:
                    for event in pygame.event.get():
                          if event.type == pygame.QUIT:
                                sys.exit()
+
+                  #redraw the screen during @ pass thru the loop
+                   self.screen.fill(self.bg_color)
 
 
                     #Make most recently drawn screen visible.
