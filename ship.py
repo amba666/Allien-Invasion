5eb82755891a458955b6c35ba89f16180a4aa392
Amba <67413568+amba666@users.noreply.github.com>
2024-03-04 13:12:10 +0300

    a-i game adding ship movements and refactoring codes

--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the position of the ship and set its start position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect
@@ -14,17 +15,21 @@ class Ship:
         # Start the new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #store a decimal value for the ships's horizontal position.
+        self.x = float(self.rect.x)
+
         #moving flag
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship position based on the flag"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
-
+        if self.moving_right and self.rect.right< self.screen_rect.right:
+            self.x +=  self.settings.ship_speed
+        if self.moving_left and self.rect > 0:
+            self.x -=  self.settings.ship_speed
+        #update rect obj from self.x.
+        self.rect.x = self.x
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
