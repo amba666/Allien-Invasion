35d2294e410ae92c4aab7aa3fd534ba7124a010f
Amba <67413568+amba666@users.noreply.github.com>
2024-03-04 13:39:55 +0300

    a-i game adding fullscreen

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,7 +11,12 @@ class AllienInvasion:
             pygame.init()
             self.settings = Settings()
 
-            self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+            #add full screen mode 
+            self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+            self.settings.screen_width = self.screen.get_rect().width
+            self.settings.screen_height = self.screen.get_rect().height
+
+            # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))if self.moving_left and self.rect > 0:
             pygame.display.set_caption("Allien Invasion")
 
             self.ship = Ship(self)
