a62943d7e92c89e2a311641786612ce70b1191a0
Amba <67413568+amba666@users.noreply.github.com>
2024-03-11 09:43:00 +0300

    a-i game add aliens and add fleet of aliens moving aliens in def directions shotting aliens

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings:
     """Store all settings for Allien Invasion."""
 
     def __init__(self):
-        """Initilize the game's settings."""
+        """Initilize the game's settings"""
         #Screen settings
         self.screen_width = 1200
         self.screen_height = 800
